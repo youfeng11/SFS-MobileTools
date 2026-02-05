f3629c6189ce608c54420516b1f62fb4cff242ee
由风 <youfeng11@outlook.com>
2026-02-05 16:10:58 +0800

    chore: 修改版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.youfeng.sfs.mobiletools"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 41
+        versionName = "0.0.1-dev"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
