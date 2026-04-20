1f789527fd4d0a12aa8eaab722614eb02c0d8806
由风 <youfeng11@outlook.com>
2026-04-20 12:23:56 +0800

    chore: 将 Navigation Compose Hilt Integration 依赖库替换为 ViewModel Compose Hilt Extension

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,7 +91,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material3.adaptive.navigation.suite)
-    implementation(libs.androdix.hilt.navigation.compose)
+    implementation(libs.androdix.hilt.lifecycle.viewmodel.compose)
     implementation(libs.androidx.navigation3.runtime)
     implementation(libs.androidx.navigation3.ui)
     implementation(libs.retrofit)
